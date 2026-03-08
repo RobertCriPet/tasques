14cb42ed6161a34ce0404eaf84c13c45184ea191
RobertCriPet <robpetpet@alu.edu.gva.es>
2026-03-08 11:31:37 +0100

    documentacion javaDOC

---
+++ Documentacion/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Cableado"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Polidivisible"},{"p":"<Unnamed>","l":"SumaDescendent"}];updateSearchResults();
